af45300844b2cbd064e37d0c33fb3c3dfed5845c
hackscribble <hackscribble@outlook.com>
2019-10-01 17:10:26 +0100

    Changed usb_context to *usb_context_ptr in calls to libusb_set_debug and libusb_set_option.

--- ftdi_usb.c
+++ ftdi_usb.c
@@ -265,9 +265,9 @@ static uint8_t ftdi_usb_init(libusb_context **usb_context_ptr, libusb_device_han
 
     /* turn on debug */
     #if LIBUSB_API_VERSION >= 0x01000106
-    libusb_set_option(usb_context, LIBUSB_LOG_LEVEL_INFO);
+    libusb_set_option(*usb_context_ptr, LIBUSB_LOG_LEVEL_INFO);
     #else
-    libusb_set_debug(usb_context, LIBUSB_LOG_LEVEL_INFO);
+    libusb_set_debug(*usb_context_ptr, LIBUSB_LOG_LEVEL_INFO);
     #endif
 
     /* now we need to decide if we are opening by VID and PID or by device number */
